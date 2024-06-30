9ebc9a4de050797809f19e1bb2ed88e17519bbe3
huyphamhaha <phamhuynhgiahuy1511@gmail.com>
2024-06-30 08:11:23 +0700

    first commit

---
+++ app/api/chat/route.ts
@@ -0,0 +1,88 @@
+import { StreamingTextResponse } from "ai";
+import { ChatMessage, MessageContent, OpenAI, TogetherLLM } from "llamaindex";
+import { NextRequest, NextResponse } from "next/server";
+import { createChatEngine } from "./engine";
+import { LlamaIndexStream } from "./llamaindex-stream";
+
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
+const convertMessageContent = (
+  textMessage: string,
+  imageUrl: string | undefined
+): MessageContent => {
+  if (!imageUrl) return textMessage;
+  return [
+    {
+      type: "text",
+      text: textMessage,
+    },
+    {
+      type: "image_url",
+      image_url: {
+        url: imageUrl,
+      },
+    },
+  ];
+};
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const { messages, data }: { messages: ChatMessage[]; data: any } = body;
+    const userMessage = messages.pop();
+    if (!messages || !userMessage || userMessage.role !== "user") {
+      return NextResponse.json(
+        {
+          error:
+            "messages are required in the request body and the last message must be from the user",
+        },
+        { status: 400 }
+      );
+    }
+
+    const llm = new TogetherLLM({
+      model: "meta-llama/Llama-3-8b-chat-hf",
+      maxTokens: 1024,
+      apiKey: process.env.TOGETHER_API_KEY,
+    });
+
+    const chatEngine = await createChatEngine(llm);
+
+    const templatePrompt =
+      "You are an assistant for question-answering tasks. Use the following pieces of retrieved context and provided data to answer the question, Absolutely do not answer questions that are not in the data. If you don't know the answer, just say that you don't know. Keep the answer concise and clear, always answer in Vietnamese, always say acording to our data";
+
+    // Convert message content from Vercel/AI format to LlamaIndex/OpenAI format
+    const userMessageContent = convertMessageContent(
+      userMessage.content.toLocaleString() + templatePrompt.toLowerCase(),
+      data?.imageUrl
+    );
+
+    // Calling LlamaIndex's ChatEngine to get a streamed response
+    const response = await chatEngine.chat({
+      message: userMessageContent,
+      chatHistory: messages,
+      stream: true,
+    });
+
+    // Transform LlamaIndex stream to Vercel/AI format
+    const { stream, data: streamData } = LlamaIndexStream(response, {
+      parserOptions: {
+        image_url: data?.imageUrl,
+      },
+    });
+
+    // Return a StreamingTextResponse, which can be consumed by the Vercel/AI client
+    return new StreamingTextResponse(stream, {}, streamData);
+  } catch (error) {
+    console.error("[LlamaIndex]", error);
+    return NextResponse.json(
+      {
+        error: (error as Error).message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/app/components/header";
+import ChatSection from "./components/chat-section";
+
+export default function Home() {
+  return (
+    <main
+      className="flex min-h-screen flex-col items-center gap-10 p-24 background-gradient"
+      style={{ height: "100vh" }}
+    >
+      <ChatSection />
+    </main>
+  );
+}
